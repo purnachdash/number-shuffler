16fb52eac6dc3f6f77f1ace9620c553bd7cea5cf
pdash <Purna.Dash@ascenaretail.com>
2020-07-27 14:41:39 +0530

    added files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { PlatformLocation } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'number-shuffler';
+  scrHeight: number;
+  scrWidth: number;
+  isBiggerScreen : boolean = true;
+  constructor( ){
+    this.getWindowSize();
+  }
+  ngOnInit(){
+
+  }
+  @HostListener('window:resize', ['$event'])
+  getWindowSize(event?) {
+         
+          this.scrHeight = window.innerHeight;
+          this.scrWidth = window.innerWidth;
+          
+          if(this.scrWidth <= 800)
+            this.isBiggerScreen = false;
+          else
+          this.isBiggerScreen = true;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NumberBlocksComponent } from './number-blocks/number-blocks.component';
+import { NumberBlocksVerticalComponent } from './number-blocks-vertical/number-blocks-vertical.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumberBlocksComponent,
+    NumberBlocksVerticalComponent
   ],
   imports: [
     BrowserModule
